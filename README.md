0fe6cd29f0572b660c1f3997c1295bc56e2c99dd
Jeroen Smink <jeroensmink98@gmail.com>
2022-07-04 17:19:06 +0200

    chore: setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# ZX JavaScript Template
+
+A starter template to create ZX scripts in your favorite language **JavaScript**
---
+++ src/index.mjs
@@ -0,0 +1,14 @@
+#!/usr/bin/env zx
+
+/**
+ * @dependencies zx
+ * zx: npm i -g zx
+ * 
+ * @docs
+ * 
+ */
+
+$.verbose = false;
+
+
+await $`${console.log("hello world")}`
